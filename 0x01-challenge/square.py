23f8532f5bec60cb5074c79bdd33e0445af9d1c4
MalekAjil <aboanasajil@gmail.com>
2024-05-28 14:53:46 -0700

    task1

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
+"""Square Model"""
+
 
 class square():
-
+    """Represents a Square"""
     width = 0
     height = 0
 
     def __init__(self, *args, **kwargs):
+        """the intializer"""
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -14,9 +17,11 @@ class square():
         return self.width * self.width
 
     def PermiterOfMySquare(self):
+        """The Pirameter of the Square"""
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """The string representation"""
         return "{}/{}".format(self.width, self.height)
 
 
